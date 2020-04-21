9ff8abccb2fd2f24c74941f83a45249111e7c52f
KhaledBelgasmi <46570185+KhaledBelgasmi@users.noreply.github.com>
2020-04-21 20:08:51 +0100

    Instantiation and more animation
    Created code to instantiate a projectile and programmatically moved it left to right. Can also do this by recording an animation. Created a variable for porjectile speed.

---
+++ TowerDefenceGame/Assets/Scripts/Shooter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectile, gun;
+    //This allows you to quickly reference GameObjects by ragging and dropping onto the script in Unity
+
+    public void Fire()
+    {
+        Instantiate(projectile, gun.transform.position, transform.rotation);
+        //projectile is the game object to be instantiated at the position of 
+        //the gun game object and with the rotation of the cactus which is zero
+    }
+}
