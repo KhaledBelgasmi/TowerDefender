a6c398b1b06c8c7996f4d3f4c3c369c515985548
KhaledBelgasmi <46570185+KhaledBelgasmi@users.noreply.github.com>
2020-05-15 19:00:05 +0100

    attacker in lane
    Implemented a system to programatically detect if there is an attacker in a specific lane. This will be linked to specific animations i.e. idle and attack.

--- TowerDefenceGame/Assets/Scripts/Shooter.cs
+++ TowerDefenceGame/Assets/Scripts/Shooter.cs
@@ -4,9 +4,57 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-    [SerializeField] GameObject projectile;
-    [SerializeField] GameObject gun;
+    [SerializeField] GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;
+   
     //This allows you to quickly reference GameObjects by dragging and dropping onto the script in Unity
+
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    public void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("Shooting");
+            //TODO change animation state to shooting
+        }
+        
+        else
+        {
+            Debug.Log("sit and wait");
+            //TODO change animation state to idle
+        }
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool IsCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+            if (IsCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        } 
+    }
 
     public void Fire()
     {
