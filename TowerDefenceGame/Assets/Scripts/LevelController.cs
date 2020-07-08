9bbe7a3e5f58a9eff8284a41ad372c7bef0dd3ab
KhaledBelgasmi <46570185+KhaledBelgasmi@users.noreply.github.com>
2020-07-08 15:58:14 +0100

    Upgrade and win experience
    Upgraded to newer version of Unity and let the system make changes to the game. Also made the win experience better by adding a message and a sound effect which are activated using a co-routine.

--- TowerDefenceGame/Assets/Scripts/LevelController.cs
+++ TowerDefenceGame/Assets/Scripts/LevelController.cs
@@ -5,9 +5,17 @@ using UnityEngine;
 public class LevelController : MonoBehaviour
 {
 
+    [SerializeField] float waitToLoad = 4f;
+    [SerializeField] GameObject winLabel;
     int numberOfattackers = 0;
     bool levelTimerFinished = false;
-    
+
+
+    private void Start()
+    {
+        winLabel.SetActive(false);
+    }
+
     public void AttackerSpawned()
     {
         numberOfattackers++;        
@@ -18,8 +26,17 @@ public class LevelController : MonoBehaviour
         numberOfattackers--;
         if (numberOfattackers <= 0 && levelTimerFinished)
         {
-            Debug.Log("End level now");
+            StartCoroutine(HandleWinCondition());
         }
+    }
+
+    IEnumerator HandleWinCondition()
+    {
+        winLabel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(waitToLoad);
+        FindObjectOfType<LevelLoader>().LoadNextScene();
+
     }
 
     public void LevelTimerFinished()
