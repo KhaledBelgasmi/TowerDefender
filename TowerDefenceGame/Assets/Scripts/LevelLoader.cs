85c0c0643f90f889ebb4ec2330432ba0b477bfde
KhaledBelgasmi <46570185+KhaledBelgasmi@users.noreply.github.com>
2020-07-24 13:43:16 +0100

    Options menu & PlayerPrefs
    Created an options menu with sliders for difficulty and volume. Created PlayerPrefs functionality for saving variables such as volume and difficulty.

--- TowerDefenceGame/Assets/Scripts/LevelLoader.cs
+++ TowerDefenceGame/Assets/Scripts/LevelLoader.cs
@@ -51,4 +51,9 @@ public class LevelLoader : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void LoadOptionsScreen()
+    {
+        SceneManager.LoadScene("OptionsScreen");
+    }
 }
