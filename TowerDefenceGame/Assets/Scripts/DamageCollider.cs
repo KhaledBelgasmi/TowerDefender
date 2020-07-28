93d7d56e793ebf17c4f771022a127fdde2320347
KhaledBelgasmi <46570185+KhaledBelgasmi@users.noreply.github.com>
2020-07-28 18:33:05 +0100

    Volume & Difficulty
    Implemented volume control and difficulty control functionality. Also corrected a bug where attackers were not destroyed upon entering the end zone collider.

--- TowerDefenceGame/Assets/Scripts/DamageCollider.cs
+++ TowerDefenceGame/Assets/Scripts/DamageCollider.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class DamageCollider : MonoBehaviour
 {
-    private void OnTriggerEnter2D()
+    private void OnTriggerEnter2D(Collider2D otherCollider)
     {
         FindObjectOfType<LivesDisplay>().TakeLife();
+        Destroy(otherCollider.gameObject);
     }
     //no argument is passed to this method because only enemies will ever hit the damage collider
     
